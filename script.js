85bdad63961fa382b26c4adbb9591d4028cebb20
Rory Gaddin <rory.gaddin@stanchionpayments.com>
2017-03-04 16:41:02 +0200

    Added some more variables

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ Date  : 04 March 2017
 
 var name = 'John';
 console.log(name);
+
+var lastName = ' Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
